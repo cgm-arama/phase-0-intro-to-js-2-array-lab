0802520135ad702884d2a9c8ae13edd546b822ce
Eric <eric.arama@student.moringaschool.com>
2023-04-12 10:07:13 +0300

    assignment done

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+cats.push("Catname");
+
+console.log(cats);
+
+cats.unshift("cat");
+
+cats.pop("Catname");
+
+cats.shift("cat");
+
+const newCats = cats.concat("Tomcat");
+
+
+
+function prependCat(cat, array) {
+  return [cat, ...array];
+}
+
+const removeFirstCat = prependCat({ name: "Simba" }, cats);
+
+console.log(newCats);
+console.log(cats); 
+
+
